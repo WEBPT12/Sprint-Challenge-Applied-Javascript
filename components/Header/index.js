9624a2dc1dd03acd3593599a26c57ead223d6a54
Mysti Dyse <mystidyse@gmail.com>
2019-12-18 17:48:48 -0800

    completed instructions in Header/index.js and created the Header compnent

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,34 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+
+    //<div class="header">
+    const divHeader = document.createElement('div')
+    divHeader.classList.add('header')
+
+    //<span class="date">SMARCH 28, 2019</span>
+    const spanDate = document.createElement('span')
+    spanDate.classList.add('date')
+    spanDate.textContent = 'SMARCH 28, 2019'
+
+    //<h1>Lambda Times</h1>
+    const h1Header = document.createElement('span')
+    h1Header.textContent = 'Lambda Times'
+
+    //<span class="temp">98°</span>
+    const spanTemp = document.createElement('span')
+    spanTemp.classList.add('temp')
+    spanTemp.textContent = '98°'
+    // creating an array of my different component pieces 
+    const components = [spanDate, h1Header, spanTemp]
+    // then using .map to loop thorugh my array appending eachone after the divHeader 
+    components.map(component => divHeader.append(component))
+
+    return divHeader
+}
+
+//And add it to the DOM in the .header-container component
+// creating a variable to hold the entire function
+const headerContainer = Header()
+document.querySelector('.header-container').appendChild(headerContainer)
