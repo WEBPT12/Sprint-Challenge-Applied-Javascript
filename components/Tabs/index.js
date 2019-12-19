9476d11e22d214b04935b40387b90562fa5864bd
Mysti Dyse <mystidyse@gmail.com>
2019-12-18 18:04:36 -0800

    succesfully pulled data from the url to show the tabs with the different topics used .map to iterate over the topics and succeded. must remember that you can call the parameter what ever you want but it must be the same in your function

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,25 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+//axos .get add the url given
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(res => {
+        const topics = res.data.topics
+        //maping over the data given for topics and adding it to the given element
+        topics.map(topic => {
+            const topicDiv = document.querySelector('.topics')
+        //by adding the parameter (topic) you can add this later so that in your function you will be pulling the topic date into the text content of the component
+        topicDiv.appendChild(newTab(topic))
+        })
+    })
+    .catch(err => console.log(err))
+
+function newTab(topic) {
+        //<div class="tab">topic here</div>
+        const divTab = document.createElement('div')
+        divTab.classList.add('tab')
+        divTab.textContent = topic
+
+        return divTab
+    }
